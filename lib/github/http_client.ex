86e633c3c377a0a4a6a58993421281a318fd254b
Derek Hubbard <derekehubbard@gmail.com>
2017-08-12 20:50:31 -0400

    Resolve Iconsistencies Between Github Clients
    Cleans up a few warnings identified with the github clients.

--- lib/github/http_client.ex
+++ lib/github/http_client.ex
@@ -1,7 +1,6 @@
-defmodule GitInpsect.Github.HttpClient do
-
-  def get_pull_requests do
+defmodule GitInspect.Github.HttpClient do
+  def get_pull_requests(_organization) do
+    # TODO: Retrieve pull requests for provided organization
     [%{id: 1, name: "this one came from github"}]
   end
-
 end
--- lib/github/local_client.ex
+++ lib/github/local_client.ex
@@ -1,5 +1,5 @@
 defmodule GitInspect.Github.LocalClient do
-  def get_pull_requests(organization) do
+  def get_pull_requests(_organization) do
     [
       %{id: 1, name: "one pull request"},
       %{id: 2, name: "another pull request"}
