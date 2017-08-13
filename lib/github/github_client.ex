4e5325c4fd7756e6292a7125141e798fa68efd49
Derek Hubbard <derekehubbard@gmail.com>
2017-08-12 21:32:34 -0400

    Adds Initial Authentication Header
    Adds an initial auth header to the github client.

--- lib/github/github_client.ex
+++ lib/github/github_client.ex
@@ -1,6 +1,10 @@
 defmodule GitInspect.Github.GithubClient do
   use HTTPoison.Base
 
+  def process_request_headers(headers) do
+    headers ++ ["Authorization": "Bearer 123"]
+  end
+
   def process_url(url) do
     "https://api.github.com/graphql" <> url
   end
--- test/github/github_client_test.exs
+++ test/github/github_client_test.exs
@@ -2,6 +2,15 @@ defmodule GitInspect.Github.GithubClientTest do
   use ExUnit.Case
   alias GitInspect.Github.GithubClient
 
+  test "adds the authorization header to the request headers" do
+    existing_headers = []
+
+    headers = GithubClient.process_request_headers(existing_headers)
+
+    assert headers |> length == 1
+    assert Keyword.has_key?(headers, :Authorization)
+  end
+
   test "builds the full url for the github graphql api" do
     base_url = Application.get_env(:git_inspect, :github_endpoint)
     route = "/test_route"
