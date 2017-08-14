30bbe6f52fb908e851d56b0c00181d24645f7a7e
Derek Hubbard <derekehubbard@gmail.com>
2017-08-13 20:04:16 -0400

    Resolve Compiler Warnings
    Resolves several compiler warnings with the mock client.

--- lib/github/mock_client.ex
+++ lib/github/mock_client.ex
@@ -1,24 +1,22 @@
 defmodule GitInspect.Github.MockClient do
   alias GitInspect.Github.MockRepo
 
-  @repo File.read("mock_repo.json")
-
   def get(url) do
     case String.split(url, "/") do
-      ["users", username, "repos"] -> repositories_list_users(username)
-      ["repos", owner, repo, "pulls"] -> pulls_list(owner, repo)
+      ["users", _username, "repos"] -> repositories_list_users()
+      ["repos", _owner, _repo, "pulls"] -> pulls_list()
       [_] -> IO.puts("Not supported yet")
     end
   end
 
-  defp repositories_list_users(username) do
+  defp repositories_list_users() do
     repositories = [
       MockRepo.get_repo()
     ]
     {:ok, %HTTPoison.Response{headers: nil, body: repositories, status_code: 200}}
   end
 
-  defp pulls_list(owner, repo) do
+  defp pulls_list() do
     pulls = [
       MockRepo.get_pull_request()
     ]
